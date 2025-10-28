955a0ace5b5cd3f308a296e4d136133f996ac4f2
Jonas <jonascanaza@gmail.com>
2025-10-28 15:16:22 -0300

    Texture with rotation is added to the projectile

--- src/entities/projectile.cpp
+++ src/entities/projectile.cpp
@@ -6,14 +6,28 @@
 
 #include "nave.h"
 #include "game/game_constants.h"
+#include <utilities/math_utils.h>
+
+using namespace MathUtils;
 
 namespace Projectile
 {
+	static Texture texture;
+
+	static const float HALF_FACTOR = 0.5f;
+	static const float MAX_ROTATION = 360.0f;
+
+	static const float RADIUS = 10.0f;
 	static const float PROJECTILE_SPEED = 1000.0f;
+
+	static const float MIN_ROTATION_RANDOM = 250.0f;
+	static const float MAX_ROTATION_RANDOM = 350.0f;
+
+	static void UpdateRotation(Projectile& projectile, float deltaTime);
 
 	void Init()
 	{
-
+		texture = LoadTexture("res/textures/entities/projectile/projectile.png");
 	}
 
 	void Update(Projectile& projectile, float deltaTime)
@@ -25,6 +39,8 @@ namespace Projectile
 
 		projectile.x += projectile.velocityX * deltaTime;
 		projectile.y += projectile.velocityY * deltaTime;
+
+		UpdateRotation(projectile, deltaTime);
 
 		if (projectile.x < 0 || projectile.x > SCREEN_WIDTH ||
 			projectile.y < 0 || projectile.y > SCREEN_HEIGHT)
@@ -40,15 +56,26 @@ namespace Projectile
 			return;
 		}
 
-		int x = static_cast<int>(projectile.x);
-		int y = static_cast<int>(projectile.y);
+		float textureWidth = static_cast<float>(texture.width);
+		float textureHeight = static_cast<float>(texture.height);
 
-		DrawCircle(x, y, projectile.radius, YELLOW);
+		float halfWidth = textureWidth * HALF_FACTOR;
+		float halfHeight = textureHeight * HALF_FACTOR;
+
+		float scaleX = projectile.radius / halfWidth;
+		float scaleY = projectile.radius / halfHeight;
+
+		Rectangle source = { 0.0f, 0.0f, textureWidth, textureHeight };
+		Rectangle dest = { projectile.x, projectile.y, textureWidth * scaleX, textureHeight * scaleY };
+		Vector2 origin = { (textureWidth * scaleX) * HALF_FACTOR, (textureHeight * scaleY) * HALF_FACTOR };
+
+		DrawTexturePro(texture, source, dest, origin, projectile.rotation, WHITE);
+		DrawCircleLines(static_cast<int>(projectile.x), static_cast<int>(projectile.y), projectile.radius, RED);
 	}
 
 	void Close()
 	{
-
+		UnloadTexture(texture);
 	}
 
 	Projectile Create(Nave::Nave& nave)
@@ -57,7 +84,7 @@ namespace Projectile
 
 		newProjectile.x = nave.x;
 		newProjectile.y = nave.y;
-		newProjectile.radius = 10.0f;
+		newProjectile.radius = RADIUS;
 		newProjectile.speedMax = PROJECTILE_SPEED;
 		newProjectile.rotation = nave.rotation;
 		newProjectile.isActive = true;
@@ -67,11 +94,29 @@ namespace Projectile
 		newProjectile.velocityX = cos(angleRad) * PROJECTILE_SPEED;
 		newProjectile.velocityY = sin(angleRad) * PROJECTILE_SPEED;
 
+		float speed = GetFloatRandomBetween(MIN_ROTATION_RANDOM, MAX_ROTATION_RANDOM);
+		int sign = (rand() % 2 == 0) ? 1 : -1;
+		newProjectile.rotationSpeed = speed * sign;
+
 		return newProjectile;
 	}
 
 	void Reset(Projectile& Projectile)
 	{
 		Projectile.isActive = false;
+	}
+
+	static void UpdateRotation(Projectile& projectile, float deltaTime)
+	{
+		projectile.rotation += projectile.rotationSpeed * deltaTime;
+
+		if (projectile.rotation > MAX_ROTATION)
+		{
+			projectile.rotation -= MAX_ROTATION;
+		}
+		else if (projectile.rotation < 0.0f)
+		{
+			projectile.rotation += MAX_ROTATION;
+		}
 	}
 }
--- src/entities/projectile.h
+++ src/entities/projectile.h
@@ -14,6 +14,7 @@ namespace Projectile
 		float radius;
 		float speedMax;
 		float rotation;
+		float rotationSpeed;
 		float velocityX;
 		float velocityY;
 		bool isActive;
