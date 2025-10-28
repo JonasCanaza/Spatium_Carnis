04c9813f7271df741ed483db40e8093e84cdcfb7
Jonas <jonascanaza@gmail.com>
2025-10-28 17:38:09 -0300

    Added description on the how to play screen

--- src/screens/how_to_play_screen.cpp
+++ src/screens/how_to_play_screen.cpp
@@ -3,17 +3,31 @@
 #include "raylib.h"
 
 #include "game/game.h"
+#include "game/game_constants.h"
+#include "interface/button.h"
+#include "interface/ui_constants.h"
+
+using namespace UIConstants;
 
 namespace HowToPlay
 {
+	static Texture descriptionTexture;
+
+	Button::Button button;
+
+	static void InitButton();
+	static void UpdateButton();
+	static void DrawButton();
+
 	void Init()
 	{
+		descriptionTexture = LoadTexture("res/textures/descriptions/how_to_play.png");
 
+		InitButton();
 	}
 
 	void Input()
 	{
-		// TEST!!!
 		if (IsKeyPressed(KEY_ESCAPE))
 		{
 			SpatiumCarnis::currentScene = SpatiumCarnis::Scenes::MainMenu;
@@ -22,7 +36,7 @@ namespace HowToPlay
 
 	void Update()
 	{
-
+		UpdateButton();
 	}
 
 	void Draw()
@@ -30,14 +44,38 @@ namespace HowToPlay
 		BeginDrawing();
 		ClearBackground(BLACK);
 
-		// TEST!!!
-		DrawText("How To Play Screen", 0, 0, 25, WHITE);
+		DrawTexture(descriptionTexture, 0, 0, WHITE);
+		DrawButton();
 
 		EndDrawing();
 	}
 
 	void Close()
 	{
+		UnloadTexture(descriptionTexture);
+	}
 
+	static void InitButton()
+	{
+		float x = static_cast<float>(SCREEN_WIDTH) / 2.0f - BUTTON_WIDTH / 2.0;
+		float buttonMarginBottom = 10.0f;
+		float y = static_cast<float>(SCREEN_HEIGHT) - BUTTON_HEIGHT - buttonMarginBottom;
+
+		button = Button::Create(x, y, BUTTON_WIDTH, BUTTON_HEIGHT, "Return to menu", Button::Type::Common);
+	}
+
+	static void UpdateButton()
+	{
+		Button::Update(button);
+
+		if (button.clicked)
+		{
+			SpatiumCarnis::currentScene = SpatiumCarnis::Scenes::MainMenu;
+		}
+	}
+
+	static void DrawButton()
+	{
+		Button::Draw(button);
 	}
 }
